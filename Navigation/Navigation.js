3c366137bcebb2dba5931c60190911a7754553b5
Florian Lück <lueck.florian@t-online.de>
2018-12-22 23:17:05 +0100

    Initial commit

---
+++ Navigation/Navigation.js
@@ -0,0 +1,9 @@
+document.write("<link rel=\"stylesheet\" href=\"../Styles/navigation.css\">\n" +
+    "\n" +
+    "    <div class=\"sidebar\">\n" +
+    "    <a href=\"../HTML_Pages/Home.html\">Home</a>\n" +
+    "    <a href=\"../HTML_Pages/Game.html\">Game</a>\n" +
+    "    <a href=\"../HTML_Pages/Scores.html\">Scores</a>\n" +
+    "    <a href=\"../HTML_Pages/Help.html\">Hilfe</a>\n" +
+    "    <a href=\"../HTML_Pages/Credits.html\">Credits</a>\n" +
+    "    </div>");
